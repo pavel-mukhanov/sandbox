c11330371b412161a1a9886f8a423f8b00c653a1
pavel-mukhanov <mukhanovpv@gmail.com>
2018-08-21 15:14:09 +0300

    Some cleaning.

--- src/lifetimes.rs
+++
@@ -1,8 +0,0 @@
-#[test]
-fn test_lifetimes() {
-    println!("foo {:?}", foo(1));
-}
-
-fn foo<'a>(x: u32) -> &'a u32 {
-    &x
-}
--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,31 @@
 #[macro_use]
 extern crate log;
-extern crate byteorder;
-extern crate env_logger;
-extern crate futures;
-extern crate partial_io;
-extern crate tokio_core;
-extern crate tokio_io;
 
-use byteorder::{ByteOrder, LittleEndian};
-use futures::future::Future;
-use std::cell::RefCell;
-use std::collections::VecDeque;
-use std::io;
-use std::io::Cursor;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use tokio_io::{codec::length_delimited::*,
-               io::{read_exact, write_all},
-               AsyncRead,
-               AsyncWrite};
+#[derive(Debug)]
+pub struct State {
+    s: String,
+}
 
-mod byte_stream;
-mod lifetimes;
+#[derive(Debug)]
+pub struct StateHolder<K = State> {
+    state: K,
+}
 
-use byte_stream::ByteStream;
-
-use byte_stream::Mock;
-use partial_io::{PartialAsyncRead, PartialOp};
-use tokio_core::reactor::Core;
+impl StateHolder {
+    pub fn new() -> StateHolder {
+        StateHolder {
+            state: State {
+                s : String::from("State"),
+            }
+        }
+    }
+}
 
 fn main() {
-    let mut data = VecDeque::new();
+    let state = StateHolder::new();
 
-    //    data.push_front(Ok(vec![0u8; 4].into()));
+    println!("state: {:#?}", state);
 
-    let mock = Mock { calls: data };
 
-    let remote_data = Arc::new(mock);
-    //    let local_data = remote_data.clone();
-
-    //    thread::spawn(move || {
-
-    //        let mut core = Core::new().unwrap();
-    //        let mock = Arc::try_unwrap(remote_data).unwrap();
-    //        let read_fut = read(mock);
-    //        let res = core.run(read_fut).unwrap();
-
-    //    });
-
-    let mut core = Core::new().unwrap();
-
-    let mock = Arc::try_unwrap(remote_data).unwrap();
-
-    let write_fut = write(mock, &vec![1u8; 4], 4).and_then(|(sock, msg)| read(sock));
-
-    let res = core.run(write_fut).unwrap();
-
-    println!("res {:?} ", res);
-}
-
-fn read<S: AsyncRead + 'static>(sock: S) -> impl Future<Item = (S, Vec<u8>), Error = io::Error> {
-    let buf = vec![0u8; 4];
-    read_exact(sock, buf).and_then(|(stream, msg)| {
-        println!("msg {:?} ", msg);
-        read_exact(stream, vec![0u8; msg[0] as usize])
-    })
-}
-
-fn write<S: AsyncWrite + 'static>(
-    sock: S,
-    buf: &[u8],
-    len: usize,
-) -> impl Future<Item = (S, Vec<u8>), Error = io::Error> {
-    let mut message = vec![0u8; 4];
-
-    LittleEndian::write_u16(&mut message, len as u16);
-    message.extend_from_slice(&buf[0..len]);
-    write_all(sock, message)
+    println!("u16 max value {:?}", <u16>::max_value())
 }
